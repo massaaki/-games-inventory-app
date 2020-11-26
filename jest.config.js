d4cbfdf6cc4d2100bda38f5134aed8002e87aadd
Massaaki <mauricio.massaaki@gmail.com>
2020-11-25 23:29:54 -0300

    feat(config/jest): Added baseUrl path

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  moduleDirectories: ['node_modules', 'src'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
