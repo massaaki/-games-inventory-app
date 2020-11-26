299e4120b2c13691542987369bc37fafca4ba164
Massaaki <mauricio.massaaki@gmail.com>
2020-11-25 22:23:12 -0300

    feat(theme): Add ThemeProvider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,15 @@
 import GlobalStyles from '../src/styles/global'
-
+import { ThemeProvider } from 'styled-components'
+import theme from '../src/styles/theme'
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
