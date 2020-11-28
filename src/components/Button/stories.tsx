7e00c3fdd71264e8951cc58d9e483fcdc2fe8e4b
Massaaki <mauricio.massaaki@gmail.com>
2020-11-28 18:31:03 -0300

    refactor(button): Adjust slyles

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -16,15 +16,21 @@ export default {
 } as Meta
 
 export const Default: Story = (args) => <Button {...args} />
-
 Default.args = {
   children: 'Buy now'
 }
 
 export const withIcon: Story = (args) => <Button {...args} />
-
 withIcon.args = {
   size: 'small',
   children: 'Buy now',
   icon: <AddShoppingCart />
+}
+
+export const asLink: Story = (args) => <Button {...args} />
+asLink.args = {
+  size: 'large',
+  children: 'Buy now',
+  as: 'a',
+  href: '/link'
 }
