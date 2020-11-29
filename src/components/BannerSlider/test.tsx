7a0336547658bac362c2b789d5d01cb8b1db5f29
Massaaki <mauricio.massaaki@gmail.com>
2020-11-29 18:31:43 -0300

    feat(GameCardSlider): Add Game Card Slider Component && Adjust media mock name

--- src/components/BannerSlider/test.tsx
+++ src/components/BannerSlider/test.tsx
@@ -1,5 +1,5 @@
 import { screen } from '@testing-library/react'
-import 'media-media-mock'
+import 'match-media-mock'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import BannerSlider from '.'
--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -1,4 +1,4 @@
-import 'media-media-mock'
+import 'match-media-mock'
 import { render, screen } from '@testing-library/react'
 
 import Slider from '.'
