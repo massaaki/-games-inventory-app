622fb3339163c9c23488c67e8f2decc914b32fe7
Massaaki <mauricio.massaaki@gmail.com>
2020-12-02 15:44:05 -0300

    test: test commit

---
+++ src/components/TesteC/stories.tsx
@@ -0,0 +1,10 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import TesteC from '.'
+
+export default {
+title: 'TesteC',
+component: TesteC
+} as Meta
+
+export const Default: Story = () =>
+<TesteC />
