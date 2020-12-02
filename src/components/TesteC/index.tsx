26d9bbbac615b8bdd891d82c1c1eb514942a3be9
Massaaki <mauricio.massaaki@gmail.com>
2020-12-02 15:44:55 -0300

    test: test commit

--- src/components/TesteC/index.tsx
+++ src/components/TesteC/index.tsx
@@ -1,9 +1,10 @@
 import * as S from './styles'
 
 const TesteC = () => (
-<S.Wrapper>
-  <h1>TesteC</h1>
-</S.Wrapper>
+  <S.Wrapper>
+    <h1>TesteC</h1>
+    <h2>Subtitle here</h2>
+  </S.Wrapper>
 )
 
 export default TesteC
