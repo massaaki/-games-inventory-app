509db436012d10966275c69bb489769a167f0260
Massaaki <mauricio.massaaki@gmail.com>
2020-12-04 23:47:52 -0300

    feat(page/auth): Add auth(Signin && Signup) page structure

---
+++ src/templates/Auth/index.tsx
@@ -0,0 +1,32 @@
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+import * as S from './styles'
+
+export type AuthProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const Auth = ({ title, children }: AuthProps) => (
+  <S.Wrapper>
+    <S.BannerBlock>
+      <Logo />
+      <Heading>All your favorite games in one place</Heading>
+      <S.Subtitle>
+        <strong>WON</strong> is the best and most complete gaming platform.
+      </S.Subtitle>
+      <S.Footer>Won Ganes 2020 © Todos os direitos reservados.</S.Footer>
+    </S.BannerBlock>
+    <S.Content>
+      <Logo size="large" />
+
+      <Heading lineColor="secondary" lineLeft>
+        {title}
+      </Heading>
+
+      {children}
+    </S.Content>
+  </S.Wrapper>
+)
+
+export default Auth
