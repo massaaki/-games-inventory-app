c013c438ff2c38f221620e0688b77d7fb891f72e
Massaaki <mauricio.massaaki@gmail.com>
2020-12-05 15:36:55 -0300

    fix(logo): Adjust logo with a dinamic id

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -4,6 +4,10 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Logo from '.'
 
 describe('<Logo />', () => {
+  it('should render the logo with id passed', () => {
+    const { container } = renderWithTheme(<Logo id="logoId" />)
+    expect(container.querySelector('#paint_linear_logoId')).toBeInTheDocument()
+  })
   it('should render a white label by default', () => {
     renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
