32d66c9759c4cfc651f529e7bacfe554d1f97929
Massaaki <mauricio.massaaki@gmail.com>
2020-12-06 00:47:08 -0300

    refactor(home): added base to header and footer

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -10,33 +10,15 @@ import Home from '.'
 
 const props = {
   banners: bannerMock,
-  newGames: [gamesMock[0]],
+  newGames: gamesMock,
   mostPopularHighlight: highlightMock,
-  mostPopularGames: [gamesMock[0]],
-  upcommingGames: [gamesMock[0]],
+  mostPopularGames: gamesMock,
+  upcommingGames: gamesMock,
   upcommingHighlight: highlightMock,
-  upcommingMoreGames: [gamesMock[0]],
-  freeGames: [gamesMock[0]],
+  upcommingMoreGames: gamesMock,
+  freeGames: gamesMock,
   freeHighlight: highlightMock
 }
-
-jest.mock('components/Menu', () => {
-  return {
-    __esModule: true,
-    default: function Mock() {
-      return <div data-testid="Mock Menu"></div>
-    }
-  }
-})
-
-jest.mock('components/Footer', () => {
-  return {
-    __esModule: true,
-    default: function Mock() {
-      return <div data-testid="Mock Footer"></div>
-    }
-  }
-})
 
 jest.mock('components/Showcase', () => {
   return {
@@ -46,6 +28,7 @@ jest.mock('components/Showcase', () => {
     }
   }
 })
+
 jest.mock('components/BannerSlider', () => {
   return {
     __esModule: true,
@@ -56,12 +39,10 @@ jest.mock('components/BannerSlider', () => {
 })
 
 describe('<Home />', () => {
-  it('should render menu and footer', () => {
+  it('should render banner and showcases', () => {
     renderWithTheme(<Home {...props} />)
 
-    expect(screen.getByTestId(/Mock Menu/i)).toBeInTheDocument()
-    expect(screen.getByTestId(/Banner Slider/i)).toBeInTheDocument()
-    expect(screen.getByTestId(/Mock Footer/i)).toBeInTheDocument()
-    expect(screen.getAllByTestId(/Mock Showcase/i)).toHaveLength(5)
+    expect(screen.getByTestId('Mock Banner Slider')).toBeInTheDocument()
+    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(5)
   })
 })
