5d1c12219e95e62a7e288208116e62e616b17f24
Massaaki <mauricio.massaaki@gmail.com>
2020-12-07 22:57:32 -0300

    fix(gameDetails): Adjust date and options format

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,13 +1,23 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import GameDetails, { GameDetailsProps } from '.'
+import mockGame from './mock'
 
 export default {
   title: 'Game/GameDetails',
   component: GameDetails,
-  args: {
-    platforms: ['windows', 'linux', 'mac']
-  },
+  args: mockGame,
   argTypes: {
+    genres: {
+      control: {
+        type: 'inline-check',
+        options: ['Role-playing', 'Action', 'Adventure']
+      }
+    },
+    releaseDate: {
+      control: {
+        type: 'date'
+      }
+    },
     platforms: {
       control: {
         type: 'inline-check',
