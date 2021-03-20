39e1c4ff541c3c0c3e9d1416958f2d0ade99f54d
Massaaki <mauricio.massaaki@gmail.com>
2021-03-20 15:04:28 -0300

    feat(api/formSignup): Integrate formSignup with api

--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -39,7 +39,9 @@ describe('<TextField />', () => {
 
   it('Changes its value when typing', async () => {
     const onInput = jest.fn()
-    render(<TextField onInput={onInput} label="TextField" name="TextField" />)
+    render(
+      <TextField onInputChange={onInput} label="TextField" name="TextField" />
+    )
 
     const input = screen.getByRole('textbox')
     const text = 'This is my new text'
@@ -56,7 +58,7 @@ describe('<TextField />', () => {
     const onInput = jest.fn()
     render(
       <TextField
-        onInput={onInput}
+        onInputChange={onInput}
         label="TextField"
         name="TextField"
         disabled
