0cb259fcd2e5938f67385e2b62916f92ca19bb6d
Massaaki <mauricio.massaaki@gmail.com>
2021-03-19 21:48:05 -0300

    feat(pages/app): Add ContextApi(Hook/UseCart) in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
+import { CartProvider } from 'hooks/use-cart'
+
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import theme from '../styles/theme'
@@ -13,18 +15,21 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
-        <Head>
-          <title>Won Games</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="Boilerplate start with Nextjs, typescript, React and styled components"
-          />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        <CartProvider>
+          <Head>
+            <title>Won Games</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta
+              name="description"
+              content="Boilerplate start with Nextjs, typescript, React and styled components"
+            />
+          </Head>
+
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </CartProvider>
       </ThemeProvider>
     </ApolloProvider>
   )
