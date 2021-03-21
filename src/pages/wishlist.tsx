85cbedf9ccf1fc79b1de260619dc8d5f8c9533a2
Massaaki <mauricio.massaaki@gmail.com>
2021-03-20 21:20:50 -0300

    feat(routes): Add ProtectedRoute

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -2,17 +2,21 @@ import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
 
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
+import protectedRoutes from 'utils/protected-routes'
+import { GetServerSidePropsContext } from 'next'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
   return <Wishlist {...props} />
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
   return {
     props: {
       games: gamesMock,
       recommendedGames: gamesMock.slice(0, 5),
-      recommendedHighlight: highlightMock
+      recommendedHighlight: highlightMock,
+      session
     }
   }
 }
